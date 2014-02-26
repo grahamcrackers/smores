affdca3a4e053e1be5d4a5b19d5fa5e4465f1406
grahamcrackers <grogers12487@gmail.com>
2014-02-26 09:52:59 -0600

    Updates

--- scripts/61a368c2.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("smoresApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("smoresApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
