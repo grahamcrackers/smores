a8375ae923660aa426108b4162b3ab8cdd8218a3
grahamcrackers <grogers12487@gmail.com>
2014-03-19 22:37:34 -0500

    Updates

--- scripts/bkgrd-colors.js
+++ scripts/bkgrd-colors.js
@@ -5,13 +5,17 @@ var myDir = angular.module('MyDirectives', []);
 myDir.directive('bgColor', function($location, $rootScope) {
     var linkFunction = function(scope, element, attributes) {
 
+
         //Fire off the function to change the back ground color after the url has been changed
         $rootScope.$on('$locationChangeSuccess', function(event, next, current){
+
+            var snapContent = $('.snap-content');
+
             if ($location.path() === '/') {
-                $('body').css({'background-color': 'blue'});
+                snapContent.css({'background-color': 'blue'});
                 console.log($location.path());
             } else {
-                $('body').css({'background-color': 'white'});
+                snapContent.css({'background-color': 'white'});
             }
 
             //Tells me the path after the change in the url
